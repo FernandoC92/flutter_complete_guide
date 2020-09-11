6fb61db7f9914f1cd9cb3d2c33cdd25ab302b617
Fernando Cordeiro <fernandoagnus1@gmail.com>
2020-09-11 18:13:45 -0300

    project

---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+      MyApp()); // runApp faz com que o nosso app seja executado, ele espera o Widget principal como parâmetro
+}
+
+// ? Ctrl + Shift + R
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionsIndex = 0;
+
+  void ansWerQuestion() {
+    setState(() {
+      questionsIndex++;
+    });
+    print(questionsIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?'
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text(questions[questionsIndex]),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: ansWerQuestion,
+            ),
+            RaisedButton(child: Text('Answer 2'), onPressed: ansWerQuestion),
+            RaisedButton(child: Text('Answer 3'), onPressed: ansWerQuestion),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+/*
+Todo widget é uma classe que possui o método build
+ build() -> faz algumas configs para transforma a sua combinação de widgets em um aplicativo real
+ Scaffold cria um design base
+ AppBar() é um widget do material.dart
+ RaisedButton -> botão com efeito de foco
+ BuildContext tem haver com a posição na arvore de widgets
+ */
+/*
+? Widget com estado:
+É necessário a implementação de 2 classe para criar um widget com estado.
+A primeira classe dever representar o widget com estado, a mesma deve estender da classe StatefulWidget.
+Já a segunda deve estender da classe State<ClasseStateFul> que deve receber como parâmetro (Generics) a classe que está estendendo de StatefulWidget.
+
+Setstate é uma função que recebe outra função.
+*/
